e9fa660d75c6c446169792fa5dcda469b936291b
kuzja086 <kozs@tlink.ru>
2020-12-11 21:59:58 +0500

    standalone

--- CMD/register-ss.bat
+++ CMD/register-ss.bat
@@ -1,8 +1,8 @@
 rem %1 - полный номер версии 1С:Предприятия
 rem %2 - номер экземпляра регистрируемого сервиса
 rem %3 - расположение конфигурационного файла сервера
-set SrvcUserName=tlinknet\kozynskiy
-set SrvcUserPwd=gjhn1743
+set SrvcUserName=
+set SrvcUserPwd=
 set SrvcName="Standalone server %2"
 set BinPath="\"C:\Program Files\1cv8\%1\bin\ibsrv.exe\" --service --config=\"%~3\""
 set Desctiption="Автономный сервер 1С:Предприятия 8.3. Копия #%2"
